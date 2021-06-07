ef34a597caea2219c0696a2b0bc0d5e0c38f3c71
1826914 <1826914@stud.hs-mannheim.de>
2021-06-07 18:34:48 +0200

    extended enddate tests

--- src/test/java/de/hs_mannheim/informatik/ct/persistence/services/RoomVisitServiceTest.java
+++ src/test/java/de/hs_mannheim/informatik/ct/persistence/services/RoomVisitServiceTest.java
@@ -23,7 +23,6 @@ import de.hs_mannheim.informatik.ct.model.CheckOutSource;
 import de.hs_mannheim.informatik.ct.model.Room;
 import de.hs_mannheim.informatik.ct.model.RoomVisit;
 import de.hs_mannheim.informatik.ct.model.Visitor;
-import de.hs_mannheim.informatik.ct.persistence.InvalidEmailException;
 import de.hs_mannheim.informatik.ct.persistence.RoomVisitHelper;
 import de.hs_mannheim.informatik.ct.persistence.repositories.RoomRepository;
 import de.hs_mannheim.informatik.ct.persistence.repositories.RoomVisitRepository;
@@ -33,7 +32,6 @@ import lombok.val;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.*;
-import org.opentest4j.AssertionFailedError;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -81,6 +79,7 @@ class RoomVisitServiceTest {
     private ArgumentCaptor<List<RoomVisit>> roomVisitCaptor;
 
     private AutoCloseable mocks;
+
     private LocalDate today = LocalDate.of(2021, Month.APRIL, 2);
     private LocalDateTime now = LocalDateTime.of(today, LocalTime.of(18, 1));
 
